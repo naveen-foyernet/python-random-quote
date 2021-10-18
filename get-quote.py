78135832d7aa504f0f21d079af6b11c17a9e70c9
Naveen Gowda <naveen@foyernet.com>
2021-10-18 13:13:52 +0530

    Hello World

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,15 @@
 def main():
-  # print("Keep it logically awesome.")
+  print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  print("all sentences are:",quotes)
+  print("first sentence is:",quotes[0])
+  print("last sentence is:",quotes[-1])
+   
 
-  #print(quotes)
+  
 
 if __name__== "__main__":
   main()
