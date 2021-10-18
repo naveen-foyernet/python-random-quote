67eb1dae542f3b4017dc61aa80b5e7ef3ae35fa9
Naveen Gowda <naveen@foyernet.com>
2021-10-18 16:16:43 +0530

    added quote

--- get-quote.py
+++ get-quote.py
@@ -5,13 +5,21 @@ def primary():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-  #print(quotes)
-  # print(quotes[0])
+  
+  f = open("quotes.txt", "a")
+  f.write("Happiness is when what you think, what you say")
+  f.close()
+  
+    #print(quotes)
+    # print(quotes[0])
+  
   #print(quotes[-1])
    
-  last = 13
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+  # last = 13
+  # rnd = random.randint(0, last)
+  # print(quotes[rnd])
+  for quote in quotes:
+   print(quote)
 
 if __name__== "__main__":
   primary()
